7bae0d762538243a92b65fdb172e6e455b9a4ee9
CruelDrool <aasmund.sater@gmail.com>
2021-07-24 20:56:00 +0200

    Tweaked the functions that checks for new updates.
    It now takes into account other plugins which are using the same hostname as Update URI.

--- recaptcha/recaptcha.php
+++ recaptcha/recaptcha.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: reCAPTCHA
 Description: reCAPTCHA for Login, Register, Lost Password, Reset Password. Supports customisation of v2 and v3. Based on <a href="https://www.shamimsplugins.com">Shamim Hasan</a>'s <a href="https://wordpress.org/plugins/advanced-nocaptcha-recaptcha">Advanced noCaptcha & invisible Captcha</a>.
-Version: 1.0.2
+Version: 1.0.3
 Author: CruelDrool
 Author URI: https://github.com/CruelDrool
 Plugin URI: https://github.com/CruelDrool/WP-reCAPTCHA
@@ -18,6 +18,8 @@ defined( 'ABSPATH' ) || exit;
 
 /**
  * Bail early if PHP version dependency is not met.
+ * Using visibility on class constants introduced in 7.1.
+ * Also using the null coalescing operator introduced in 7.0.
  */
 if ( version_compare( PHP_VERSION, '7.1', '<' ) ) {
 	return;
@@ -26,11 +28,18 @@ if ( version_compare( PHP_VERSION, '7.1', '<' ) ) {
 /**
  * Plugin header ("Update URI") introduced in WordPress 5.8.
  */
-function update_plugin( $array, $plugin_data, $plugin_file, $locales){
-	$request = wp_remote_get($plugin_data['UpdateURI']);
-	$request_body = wp_remote_retrieve_body( $request );
-	$result = json_decode( $request_body, true );
-	return $result;
+function update_plugin( $update, $plugin_data, $plugin_file, $locales){
+	/*
+	This filter is applied inside a foreach loop in wp_update_plugins(). 
+	So, if there a several plugins using the same hostname as Update URI, our function will be run for each of those other plugins.
+	Better check if the loop has reach *our* plugin until we do anything.
+	*/
+	if ($plugin_file == 'recaptcha/recaptcha.php') {
+		$request = wp_remote_get($plugin_data['UpdateURI']);
+		$request_body = wp_remote_retrieve_body( $request );
+		$update = json_decode( $request_body, true );
+	}
+	return $update;
 }
 
 add_filter('update_plugins_github.com', 'CD\recaptcha\update_plugin', 10, 4);
